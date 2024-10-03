ca0dc3db5d24676c64ac727cd94956147a9f0ca2
aaherdi <aaherdi832@gmail.com>
2024-10-03 16:56:53 +0700

    upload

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProdiController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    //ROUTE PROFIL
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //ROUTE PRODI
+    Route::get('/prodi', [ProdiController::class, 'index'])->name('prodi.');
+});
+
+require __DIR__ . '/auth.php';
